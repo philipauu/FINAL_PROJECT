47fc331e249adf9bb5fbdd68e0e735492a954c88
philipauu <Phil@Phils-MacBook.local>
2017-12-07 15:53:21 +0800

    Electrocution, n.: 	Burning at the stake with all the modern improvements.

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,39 @@
 console.log('loaded map');
 
+// FUNCTION TO EDIT INFOWINDOW
+
+function edit_marker_name(a, thiis) {
+    var edit_marker_name = prompt("Edit marker name : " + $(thiis).html());
+
+    marker = window['marker_' + a];
+
+    if (edit_marker_name) {
+        $(thiis).html(edit_marker_name);
+
+        marker.setTitle(edit_marker_name);
+
+        google.maps.event.clearListeners(maker, 'click');
+
+        var content = "<span class='address_name' onclick='edit_marker_name(\"" + a + "\", this)'>" + edit_marker_name + "</span>";
+        var infowindow = new google.maps.InfoWindow();
+        google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+            return function () {
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            };
+        })(marker, content, infowindow));
+    }
+}
+
+// LOAD MAP
+
 var default_location = {
     lat: 22.3964,
     lng: 114.1095
 };
 
 function initMap() {
-    //console.log(data);
+    console.log(data);
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: default_location,
@@ -31,6 +58,8 @@ function initMap() {
 
     }
 
+    // AUTOCOMPLETE SEARCHBAR 
+
     var input = document.getElementById('pac-input');
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
@@ -47,8 +76,45 @@ function initMap() {
             map.setZoom(17);
         }
 
-        //ADD INFO WINDOW
+        //CREATING MARKER BY CLICKING
 
+        var marker_name = 'null';
+
+        google.maps.event.addListener(map, 'click', function (event) {
+
+            if (marker_name === 'null') {
+                marker_name = 'a';
+            } else {
+                marker_name = String.fromCharCode(marker_name.charCodeAt(0) + 1);
+            }
+
+            //ADDING PARAMETER MARKER
+
+            icons = 'http://maps.google.com/mapfiles/ms/micons/red-dot.png';
+            var location = event.latLng;
+            window['marker_' + marker_name] = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: icons,
+                draggable: true,
+                title: '' + marker_name
+            });
+
+            //ID MARKER
+
+            var marker = window['marker_' + marker_name];
+
+            //EVENT TO SHOW MARKER TITLE
+
+            var content = "<span class='address_name' onclick='edit_marker_name (\"" + marker_name + "\", this)'>" + marker_name + "</span>";
+            var infowindow = new google.maps.InfoWindow();
+            google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+                return function () {
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                };
+            })(marker, content, infowindow));
+        });
 
     });
 };
